0c21aad412e04f5290555b18ed04e6c49e83afbc
Sam <Sam.V_@hotmail.com>
2020-11-22 23:43:43 +1000

    Update ItemDatabase.cs

--- Assets/Scripts/SimpleInventorySystem/Classes/ItemDatabase.cs
+++ Assets/Scripts/SimpleInventorySystem/Classes/ItemDatabase.cs
@@ -23,16 +23,7 @@ namespace SimpleInventorySystem
             IsGenerated = true;
         }
 
-        public static void TestSaveDatabase()
-        {
-            File.WriteAllText(Application.dataPath + "/Resources/Test_ItemDB.json", JsonMapper.ToJson(Items));
-        }
-
-        public static void TestPrintDatabase()
-        {
-            Debug.Log(Items[0].LimitType);
-        }
-
+        #region Item Fetching
         public static Item FetchItemByID(int ID)
         {
             return Items[ID];
@@ -50,6 +41,56 @@ namespace SimpleInventorySystem
             }
             return retItem;
         }
+        #endregion
+
+        #region Fetching Recipes
+        public static List<Item> FetchRecipesByFactory(string FactoryType)
+        {
+            List<Item> retItems = new List<Item>();
+            foreach (Item _item in Items)
+            {
+                if (_item.Craftable && _item.RequiredFactory == FactoryType)
+                {
+                    //Item is craftable at the right factory.
+                    retItems.Add(_item);
+                }
+            }
+            return retItems;
+        }
+
+        public static List<Item> FetchRecipesByItem(Item item, string FactoryType)
+        {
+            List<Item> retItems = new List<Item>();
+            foreach (Item _item in retItems)
+            {
+                if (_item.Craftable && _item.RequiredFactory == FactoryType)
+                {
+                    //Item is craftable at the right factory.
+                    foreach (KeyValuePair<string, int> _ingredient in _item.Ingredients)
+                    {
+                        if (_ingredient.Key == item.Name)
+                        {
+                            // item is an ingredient of _item
+                            retItems.Add(_item);
+                        }
+                    }
+                }
+            }
+            return retItems;
+        }
+        #endregion
+
+        #region Testing
+        public static void TestSaveDatabase()
+        {
+            File.WriteAllText(Application.dataPath + "/Resources/Test_ItemDB.json", JsonMapper.ToJson(Items));
+        }
+
+        public static void TestPrintDatabase()
+        {
+            Debug.Log(Items[0].LimitType);
+        }
+        #endregion
     }
 }
 
